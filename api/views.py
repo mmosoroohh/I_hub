8bdd2eea2876f2d3fc44fd014090f5d61eff0779
mmosoroohh <arnoldmaengwe@gmail.com>
2020-03-11 12:35:16 +0300

    first commit.

--- api/views.py
+++ api/views.py
@@ -17,6 +17,7 @@ def get_asset(name):
             status=status.HTTP_404_NOT_FOUND
         )
 
+
 class ListCreateAsset(generics.ListAPIView):
     """Provides a GEt and POST method handler."""
     queryset = Assets.objects.all()
@@ -26,10 +27,10 @@ class ListCreateAsset(generics.ListAPIView):
         import pdb;
         pdb.set_trace()
         asset = Assets.objects.create(
-            asset_name = request.data["asset_name"],
-            location = request.data["location"],
-            duration = request.data["duration"],
-            process = request.data["process"]
+            asset_name=request.data["asset_name"],
+            location=request.data["location"],
+            duration=request.data["duration"],
+            process=request.data["process"]
         )
         return Response(data=AssetSerializer(asset).data, status=status.HTTP_201_CREATED)
 
@@ -78,8 +79,8 @@ class ListCreateProcessView(generics.ListAPIView):
 
     def post(self, request, *args, **kwargs):
         process = Process.objects.create(
-            process_name = request.data["process_name"],
-            owner = request.data["owner"]
+            process_name=request.data["process_name"],
+            owner=request.data["owner"]
         )
         return Response(
             data=ProcessSerializer(process).data,
@@ -88,76 +89,35 @@ class ListCreateProcessView(generics.ListAPIView):
 
 
 class ProcessDetailView(generics.RetrieveUpdateDestroyAPIView):
-     queryset = Process.objects.all()
-     serializer_class = ProcessSerializer
+    queryset = Process.objects.all()
+    serializer_class = ProcessSerializer
 
-     def get(self, request, *args, **kwargs):
-         try:
-             process = self.queryset.get(pk=kwargs["pk"])
-             return Response(ProcessSerializer(process).data)
-         except Process.DoesNotExist:
-             return Response(
-                 data={"message": "Process with id: {} does not exist".format(kwargs["pk"])},
-                 status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, *args, **kwargs):
+        try:
+            process = self.queryset.get(pk=kwargs["pk"])
+            return Response(ProcessSerializer(process).data)
+        except Process.DoesNotExist:
+            return Response(
+                data={"message": "Process with id: {} does not exist".format(kwargs["pk"])},
+                status=status.HTTP_404_NOT_FOUND)
 
-        def put(self, request, *args, **kwargs):
-            try:
-                process = self.queryset.get(pk=kwargs["pk"])
-                serializer=ProcessSerializer()
-                update_process= serializer.update(process, request.data)
-                return Response(ProcessSerializer(update_process).data)
-            except Process.DoesNotExist:
-                return Response(
-                    data={"message": "Process with id: {} does not exist".format(kwargs["pk"])},
-                    status=status.HTTP_404_NOT_FOUND)
+    def put(self, request, *args, **kwargs):
+        try:
+            process = self.queryset.get(pk=kwargs["pk"])
+            serializer = ProcessSerializer()
+            update_process = serializer.update(process, request.data)
+            return Response(ProcessSerializer(update_process).data)
+        except Process.DoesNotExist:
+            return Response(
+                data={"message": "Process with id: {} does not exist".format(kwargs["pk"])},
+                status=status.HTTP_404_NOT_FOUND)
 
-        def delete(self, request, *args, **kwargs):
-            try:
-                process = self.queryset.get(pk=kwargs["pk"])
-                process.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            except Process.DoesNotExist:
-                return Response(
-                    data={"message": "Process with id: {} does not exist".format(kwargs["pk"])},
-                    status=status.HTTP_404_NOT_FOUND)
-
-
-# class RegisterUsers(generics.CreateAPIView):
-#     """
-#     POST users/register/
-#     """
-#     permission_classes = (permissions.AllowAny,)
-#     serializer_class = UserSerializer
-#
-#     def post(self, request, *args, **kwargs):
-#         request.data['username'] = request.data['username'].lower()
-#         user = request.data
-#         serializer = self.serializer_class(
-#             data=user, context={'request': request})
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(
-#             serializer.data,
-#             status=status.HTTP_201_CREATED
-#         )
-#
-#
-# class LoginView(generics.CreateAPIView):
-#     """
-#     POST users/login/
-#     """
-#
-#     # This permission class will over ride the global permission
-#     # class setting
-#     permission_classes = (permissions.AllowAny,)
-#     serializer_class = UserSerializer
-#
-#     def post(self, request):
-#         user = request.data
-#         serializer = self.serializer_class(data=user)
-#         serializer.is_valid(raise_exception=True)
-#
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-
+    def delete(self, request, *args, **kwargs):
+        try:
+            process = self.queryset.get(pk=kwargs["pk"])
+            process.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Process.DoesNotExist:
+            return Response(
+                data={"message": "Process with id: {} does not exist".format(kwargs["pk"])},
+                status=status.HTTP_404_NOT_FOUND)
